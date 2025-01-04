ae3cdb00d878822540ca5fff8fd6a1caa8d0b7c7
lucianatux <lucianatux@yahoo.com.ar>
2025-01-04 18:22:48 -0300

    blend tree animations

---
+++ TopDown/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float moveSpeed;
+  private bool isMoving;
+  private Vector2 input;
+  private Animator animator;
+
+  private void Awake()
+  {
+    animator= GetComponent<Animator>();
+  }
+
+  private void Update()
+  {
+    // If not moving, get input
+    // Si no se está moviendo, obtiene la entrada
+    if (!isMoving)
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("moveX", input.x);
+        animator.SetFloat("moveY", input.y);
+
+        // No diagonal movement
+        // Evita el movimiento diagonal
+        if (input.x != 0) input.y = 0;
+
+        // If input exists, calculate target position
+        // Si hay entrada, calcula la posición objetivo
+        if (input != Vector2.zero)
+        {
+            var targetPos = transform.position;
+            targetPos.x += input.x;
+            targetPos.y += input.y;
+
+            StartCoroutine(Move(targetPos)); // Start moving
+            // Inicia el movimiento
+        }
+    }
+  }
+
+  IEnumerator Move(Vector3 targetPos)
+  {
+    isMoving = true; // Start movement
+    // Inicia el movimiento
+
+    while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+        // Mueve hacia el objetivo
+        yield return null;
+    }
+
+    transform.position = targetPos; // Snap to target
+    // Ajusta a la posición objetivo
+
+    isMoving = false; // End movement
+    // Finaliza el movimiento
+  }
+}
